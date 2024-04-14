c8567548afa2b894c7efbaa9394810e7a6713c1b
mdua167 <manyadua5@gmail.com>
2024-04-13 23:33:32 -0500

    landingpage

--- src/routes/LandingPage.tsx
+++ src/routes/LandingPage.tsx
@@ -16,27 +16,3 @@ export function LandingPage ()
 	);
 }
 export default LandingPage;
-
-
-/*
-
-import { Text, Image, HStack} from "@chakra-ui/react";
-import { NavBar } from "./NavBar";
-import { logoStyle } from "../customTheme";
-
-export function LandingPage () 
-{
-	return(
-		<>   
-        {}
-        <NavBar /> 
-        <Text variant="baseStyle">Welcome to Reflections | Projections 2024 ! </Text>
-        <HStack spacing="20px" style={logoStyle}>
-                <Image src="rplogo.png" alt="R|P LOGO alt" boxSize="60px" />
-        </HStack>
-		</>
-	);
-}
-export default LandingPage;
-
-*/ 
