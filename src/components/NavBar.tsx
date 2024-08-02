924de0648945dd58a1b896197c1fff17340e0cc2
anandani4136 <ra7353@hotmail.com>
2024-08-02 00:05:23 -0500

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { HStack, Image} from '@chakra-ui/react';
 export function NavBar() {
 	return (
 		<HStack justify="space-between" width="100%" position='absolute' backgroundColor='#00456D' borderBottom='solid 3px #003B5C'>
-			<Image src="/logo.svg" alt="R|P LOGO alt" height="auto" maxW="58px" padding="0.5rem" />
+			<Image src="/rp_logo.svg" alt="R|P LOGO alt" height="auto" maxW="58px" padding="0.5rem" />
 			{/* <Tabs align = 'end' variant = "unstyled" color = "white">
 				<TabList>
 					<Tab>About Us</Tab>
