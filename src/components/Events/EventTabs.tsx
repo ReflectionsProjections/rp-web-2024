ac68e8dfb7413fc841d6e8c523eece1dd6afdac7
divinedab <divyack2@illinois.edu>
2024-08-10 13:40:52 -0500

    removed the line under the tabs

--- src/components/Events/EventTabs.tsx
+++ src/components/Events/EventTabs.tsx
@@ -1,20 +1,28 @@
-import { Flex, Tab, TabList, TabPanel, TabPanels, Tabs, useColorModeValue, VStack } from "@chakra-ui/react"
+import { Flex, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
 
 import EventCard from "./EventCard";
 import EventTab from "./EventTab"
 
 export function EventTabs() {
     return (
-      <Tabs>
-        <TabList>
-          <EventTab day="WED"/>
-          <EventTab day="THUR"/>
-          <EventTab day="FRI"/>
-          <EventTab day="SAT"/>
-          <EventTab day="SUN"/>
-        </TabList>
+      <Tabs variant='unstyled'>
+        <Flex 
+          flexDirection="column"
+          alignItems="center"
+          height="100%" 
+          width="100%">
 
-        <TabPanels p='2rem'>
+            <TabList>
+              <EventTab day="WED"/>
+              <EventTab day="THUR"/>
+              <EventTab day="FRI"/>
+              <EventTab day="SAT"/>
+              <EventTab day="SUN"/>
+            </TabList>
+
+        </Flex>
+
+        <TabPanels>
           <TabPanel alignItems="center">
             <Flex 
               flexDirection="column"
@@ -22,6 +30,7 @@ export function EventTabs() {
               height="100%" 
               width="100%">
 
+              {/* Sample cards here—we need to call the GET events API endpoint in these panels. */}
               <EventCard title="Event Title" location="New York, NY" time="10:00 AM" />
               <EventCard title="Event Title" location="New York, NY" time="10:00 AM" />
               <EventCard title="Event Title" location="New York, NY" time="10:00 AM" />
