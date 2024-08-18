7717657193faf2d2192b35d247c26fb440be1e9d
divinedab <divyack2@illinois.edu>
2024-08-18 14:55:21 -0500

    saving select input

--- src/components/Registration/inputs/SelectInput.tsx
+++ src/components/Registration/inputs/SelectInput.tsx
@@ -12,9 +12,9 @@ export const SelectInput: React.FC<FormInputProps> = ({ id, name, formik, option
 	return (
 		<FormControl isInvalid={!!(formik.touched[name] && formik.errors[name])}>
 			<Select mt={2} w="100%" border={'2px solid white'} id={id} onChange={(e) => {
-				formik.setFieldValue(name, e.target.value);}} onBlur={formik.handleBlur}>
+				formik.setFieldValue(name, e.target.value);}} value={formik.values[name]} onBlur={formik.handleBlur}>
 				<option> Select Graduation Year! </option>
-				{options.map((x) => <option key={x}> {x} </option>)}
+				{options.map((x) => <option key={x} value={x}> {x} </option>)}
 			</Select>
 			<FormErrorMessage>{formik.errors[name]?.toString()}</FormErrorMessage>
 		</FormControl>
