02d1f505621163d48958dac49fa5eaaa9f17ef4c
Aydan Pirani <aydanpirani@example.com>
2024-08-26 18:05:00 -0500

    rem alert

--- src/routes/Registration.tsx
+++ src/routes/Registration.tsx
@@ -68,9 +68,7 @@ export default function Registration() {
 			if (!jwt) {
 				fetchJwt();
 			}
-			if (jwt) {
-				window.confirm("You are about to access a beta version of the registration form. Please do not submit any real data. This data will be erased prior to our official registration launch.");
-			}
+
 			setNeedsInit(false);
 		}
 	}, [needsInit, jwt]);
