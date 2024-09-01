02b4059e689371fcf309e933b0c83d42604a15c0
anandani4136 <ra7353@hotmail.com>
2024-09-01 01:27:30 -0500

    :art: New components!

---
+++ src/components/Registration/inputs/MultiLinkInput.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import {
+	Box,
+	Button,
+	FormControl,
+	FormErrorMessage,
+	Input,
+	VStack,
+	Text,
+	IconButton,
+	useMediaQuery,
+} from "@chakra-ui/react";
+import { FormikProps } from "formik";
+import { CloseIcon } from '@chakra-ui/icons';
+
+interface MultiLinkInputProps {
+  id: string;
+  name: string;
+  formik: FormikProps<any>;
+}
+
+export const MultiLinkInput: React.FC<MultiLinkInputProps> = ({
+	id,
+	name,
+	formik,
+}) => {
+	const [links, setLinks] = useState<string[]>(formik.values[name]?.map((link: string) => link.startsWith('https://') ? link : `https://${link}`) || ['https://']);
+	const [localErrors, setLocalErrors] = useState<string[]>([]);
+	const [isNarrowScreen] = useMediaQuery("(max-width: 30em)");
+
+	const MAX_LINKS = 5;
+
+	useEffect(() => {
+		if (formik.errors[name] && Array.isArray(formik.errors[name])) {
+			setLocalErrors(formik.errors[name] as string[]);
+		} else {
+			setLocalErrors([]);
+		}
+	}, [formik.errors, name]);
+
+	const handleLinkChange = (index: number, value: string) => {
+		const newLinks = [...links];
+		if (value.startsWith('https://')) {
+			newLinks[index] = value;
+		} else if (!value.startsWith('https://') && links[index] === 'https://') {
+			newLinks[index] = 'https://';
+		} else {
+			newLinks[index] = 'https://' + value.slice(value.indexOf('//') + 2);
+		}
+		setLinks(newLinks);
+		formik.setFieldValue(name, newLinks);
+	};
+
+	const addLink = () => {
+		if (localErrors.length === 0 && links.length < MAX_LINKS) {
+			setLinks([...links, 'https://']);
+			formik.setFieldValue(name, [...links, 'https://']);
+		}
+	};
+
+	const removeLink = (index: number) => {
+		const newLinks = links.filter((_, i) => i !== index);
+		setLinks(newLinks);
+		formik.setFieldValue(name, newLinks);
+	};
+
+	const RemoveButton = ({ index }: { index: number }) => (
+		isNarrowScreen ? (
+			<IconButton
+				aria-label="Remove link"
+				icon={<CloseIcon />}
+				size="sm"
+				position="absolute"
+				right="1"
+				mt='4px'
+				onClick={() => removeLink(index)}
+			/>
+		) : (
+			<Button
+				size="sm"
+				position="absolute"
+				right="1"
+				mt='4px'
+				onClick={() => removeLink(index)}
+			>
+        Remove
+			</Button>
+		)
+	);
+
+	return (
+		<FormControl isInvalid={!!(formik.touched[name] && formik.errors[name])}>
+			<VStack spacing={4} align="stretch">
+				{links.map((link, index) => (
+					<Box key={index} position="relative">
+						<Input
+							id={`${id}-${index}`}
+							value={link}
+							onChange={(e) => handleLinkChange(index, e.target.value)}
+							onBlur={formik.handleBlur}
+							placeholder="Enter website link"
+							pr={isNarrowScreen ? "3rem": "7em"}
+							isInvalid={!!(localErrors[index])}
+						/>
+						<RemoveButton index={index} />
+						{localErrors[index] && (
+							<Text color="red.500" fontSize="sm" mt={1}>
+								{localErrors[index]}
+							</Text>
+						)}
+					</Box>
+				))}
+				{links.length < MAX_LINKS && (
+					<Button 
+						onClick={addLink} 
+						colorScheme="blue"
+						isDisabled={localErrors.length > 0}
+					>
+            Add another link
+					</Button>
+				)}
+			</VStack>
+			{typeof formik.errors[name] === 'string' && (
+				<FormErrorMessage>
+					{formik.errors[name] as string}
+				</FormErrorMessage>
+			)}
+		</FormControl>
+	);
+};
--- src/components/Registration/inputs/SelectInput.tsx
+++ src/components/Registration/inputs/SelectInput.tsx
@@ -11,10 +11,35 @@ interface FormInputProps {
 export const SelectInput: React.FC<FormInputProps> = ({ id, name, formik, options }) => {
 	return (
 		<FormControl isInvalid={!!(formik.touched[name] && formik.errors[name])}>
-			<Select mt={2} w="100%" border={'2px solid white'} id={id} onChange={(e) => {
-				formik.setFieldValue(name, e.target.value);}} value={formik.values[name]} onBlur={formik.handleBlur}>
-				<option> Select Graduation Date! </option>
-				{options.map((x) => <option key={x} value={x}> {x} </option>)}
+			<Select
+				mt={2}
+				w="100%"
+				border={'2px solid white'}
+				id={id}
+				onChange={(e) => formik.setFieldValue(name, e.target.value)}
+				value={formik.values[name]}
+				onBlur={formik.handleBlur}
+			>
+				<option
+					style={{
+						color: 'white',
+						backgroundColor: '#313848',
+					}}
+				>
+      Select Graduation Date!
+				</option>
+				{options.map((x) => (
+					<option
+						key={x}
+						value={x}
+						style={{
+							color: '#CED5E4', // Light text color
+							backgroundColor: '#313848', // Dark background color
+						}}
+					>
+						{x}
+					</option>
+				))}
 			</Select>
 			<FormErrorMessage>{formik.errors[name]?.toString()}</FormErrorMessage>
 		</FormControl>
