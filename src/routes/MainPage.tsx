470ca42ef897e36c094b1167e968b7b508c017de
Dev Patel <devpatel05@gmail.com>
2024-09-01 21:35:03 -0500

    build error

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Home/Landing';
 import Info from '../components/Home/Info';
 import Events from '../components/Home/Events';
 import FAQ from '../components/Home/FAQ';
-import Sponsors from '../components/Home/Sponsors';
+// import Sponsors from '../components/Home/Sponsors';
 import Footer from "../components/Footer";
 
 export default function MainPage() {
