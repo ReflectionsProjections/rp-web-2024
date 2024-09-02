22f6abf04a3db46ddb912b699be4d7231caa5cc8
Aydan Pirani <aydanpirani@example.com>
2024-09-02 00:17:02 -0500

    Code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,23 +15,6 @@ export function NavBar({showAuth=false}: NavBarProps) {
 		<HStack justify="space-between" width="100%" position='absolute' backgroundColor='#00456D' borderBottom='solid 3px #003B5C'>
 			<Image onClick={() => window.location.href = '/'} src="/rp_logo.svg" alt="R|P LOGO alt" height="auto" maxW="58px" padding="0.5rem" cursor={"pointer"} transition={'ease-in 0.2s'} _hover={{ transform: 'scale(1.2)' }}/>
 			<Text fontWeight={"900"} size={"sm"}> REGISTRATION OPENS ON WEDNESDAY, SEPTEMBER 4. </Text>
-			{/* <Tabs align = 'end' variant = "unstyled" color = "white">
-				<TabList>
-					<Tab>About Us</Tab>
-					<Tab>Speakers</Tab>
-					<Tab>PuzzleBang</Tab>
-					<Tab>Mechmania</Tab>
-				</TabList>
-				<TabPanels>
-					<TabPanel>
-					</TabPanel>
-					<TabPanel>
-			
-					</TabPanel>
-					<TabPanel>
-					</TabPanel>
-				</TabPanels>
-			</Tabs> */}
 
 			{showAuth ? (
 				<Menu>
