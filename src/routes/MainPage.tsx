020c755e4fb604572f5d54553b1d0e4d350d5357
anandani4136 <ra7353@hotmail.com>
2024-09-13 00:01:26 -0500

    Added Sponsors :sparkles:

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Home/Landing';
 import Info from '../components/Home/Info';
 // import Events from '../components/Home/Events';
 import FAQ from '../components/Home/FAQ';
-// import Sponsors from '../components/Home/Sponsors';
+import Sponsors from '../components/Home/Sponsors';
 import Footer from "../components/Footer";
 import Events from './Events';
 
@@ -16,7 +16,7 @@ export default function MainPage() {
 			<Info />
 			<Events />
 			<FAQ/>
-			{/* <Sponsors/> */}
+			<Sponsors/>
 			<Footer />
 		</VStack>
 		
